35545262a6dc91241c952b7a76f9711ee8a23df1
Bryan Siegel <bryansiegel@gmail.com>
2024-05-16 10:24:52 -0700

    initial commit

---
+++ src/main/java/com/bryansiegel/springfileuploadnodb/SpringFileUploadNoDbApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.springfileuploadnodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileUploadNoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileUploadNoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryansiegel/springfileuploadnodb/SpringFileUploadNoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.springfileuploadnodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileUploadNoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
